abf084424a1d44fc2aded64457eddcbc35bdd281
Yoon5 <54830536+Yoon5@users.noreply.github.com>
2020-01-10 21:45:40 +0900

    Update ros2_install.sh

--- ros2_install.sh
+++ ros2_install.sh
@@ -1,5 +1,5 @@
 # ROS2 one-line install
-# Copyright Yoon Ho Seol
+# Yoon Ho Seol
 
 set -x
 
@@ -95,6 +95,33 @@ sh -c "echo \"source ~/$name_catkinws/devel/setup.bash\" >> ~/.bashrc"
 sh -c "echo \"export ROS_MASTER_URI=http://localhost:11311\" >> ~/.bashrc"
 sh -c "echo \"export ROS_HOSTNAME=localhost\" >> ~/.bashrc"
 
+
+echo "[=================]"
+
+echo "[Set the Locale]"
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+echo "[Add ROS2 Repository]"
+sudo apt update -y && sudo apt install curl gnupg2 lsb-release -y
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
+
+echo "[Install ROS2 Dashing Desktop]"
+sudo apt update -y
+sudo apt install ros-dashing-desktop -y
+source /opt/ros/dashing/setup.bash
+sudo apt install python3-argcomplete -y
+
+echo "[Set the ROS2 Environment]"
+sh -c "echo \"source /opt/ros/dashing/setup.bash\" >> ~/.bashrc"
+
+echo "[Install Turtlesim]"
+sudo apt install ros-dashing-turtlesim -y
+
+
+echo "[=================]"
 echo "[Complete!!!]"
 
 exec bash
