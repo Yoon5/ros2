f33e8d22c2e13f97b230a96ccb78237f54e00436
Yoon5 <54830536+Yoon5@users.noreply.github.com>
2020-01-11 01:42:15 +0900

    Update ros2_install.sh

--- ros2_install.sh
+++ ros2_install.sh
@@ -18,15 +18,15 @@ case $version in
   "bionic" | "xenial")
   ;;
   *)
-    echo "ERROR: This script will only work on Ubuntu Bionic Beaver (18.04) and Xenial Xerus (16.04). Exit."
+    echo "ERROR: This script will only work on Ubuntu Bionic Beaver (18.04) and Xenial Xerus (16.04). Exit." # Xenial Xerus would be deleted after Crystal 
     exit 0
 esac
 
 echo "[Set the target OS, ROS version and name of catkin workspace]"
 name_os_version=${name_os_version:="bionic"}
 name_ros_version=${name_ros_version:="dashing"}
-# name_ros_version=${name_ros_version:="oxy"}
-name_catkin_workspace=${name_catkin_workspace:="catkin_ws"}
+# name_ros_version=${name_ros_version:="foxy"}
+# name_catkin_workspace=${name_catkin_workspace:="catkin_ws"} # catkin deleted
 
 echo "[Update the package lists and upgrade them]"
 sudo apt-get update -y
@@ -71,18 +71,12 @@ echo "[Environment setup and getting rosinstall]"
 source /opt/ros/$name_ros_version/setup.sh
 sudo apt-get install -y python-rosinstall
 
-echo "[Make the catkin workspace and test the catkin_make]"
-mkdir -p $HOME/$name_catkin_workspace/src
-cd $HOME/$name_catkin_workspace/src
-catkin_init_workspace
-cd $HOME/$name_catkin_workspace
-catkin_make
-
-echo "[Set the ROS evironment]"
-sh -c "echo \"alias eb='nano ~/.bashrc'\" >> ~/.bashrc"
-sh -c "echo \"alias sb='source ~/.bashrc'\" >> ~/.bashrc"
-sh -c "echo \"alias gs='git status'\" >> ~/.bashrc"
-sh -c "echo \"alias gp='git pull'\" >> ~/.bashrc"
+# echo "[Make the catkin workspace and test the catkin_make]"  # catkin deleted
+# mkdir -p $HOME/$name_catkin_workspace/src
+# cd $HOME/$name_catkin_workspace/src
+# catkin_init_workspace
+# cd $HOME/$name_catkin_workspace
+# catkin_make
 
 
 # Added - Setup Locale, Sources, & Install packages
@@ -91,7 +85,7 @@ sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 
-echo "[Add ROS2 Sources/ apt repository]"
+echo "[Setup ROS2 apt repository]"
 sudo apt update -y && sudo apt install curl gnupg2 lsb-release -y
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
@@ -104,26 +98,26 @@ sudo apt install python3-argcomplete -y
 
 
 # Added - Edit bashrc
-echo "[Set the ROS evironment]"
-sh -c "echo \"alias eb='nano ~/.bashrc'\" >> ~/.bashrc"
+echo "[Setup bashrc; ROS evironment]"
+sh -c "echo \"alias eb='nano ~/.bashrc'\" >> ~/.bashrc"  # alias commands for bash
 sh -c "echo \"alias sb='source ~/.bashrc'\" >> ~/.bashrc"
 sh -c "echo \"alias gs='git status'\" >> ~/.bashrc"
 sh -c "echo \"alias gp='git pull'\" >> ~/.bashrc"
-# sh -c "echo \"alias cw='cd ~/$name_catkin_workspace'\" >> ~/.bashrc"
+# sh -c "echo \"alias cw='cd ~/$name_catkin_workspace'\" >> ~/.bashrc"  # catkin deleted
 # sh -c "echo \"alias cs='cd ~/$name_catkin_workspace/src'\" >> ~/.bashrc"
 # sh -c "echo \"alias cm='cd ~/$name_catkin_workspace && catkin_make'\" >> ~/.bashrc"
 
 sh -c "echo \"source /opt/ros/$name_ros_version/setup.bash\" >> ~/.bashrc"
-# sh -c "echo \"source ~/$name_catkin_workspace/devel/setup.bash\" >> ~/.bashrc"
+# sh -c "echo \"source ~/$name_catkin_workspace/devel/setup.bash\" >> ~/.bashrc"  # catkin deleted
 
-# sh -c "echo \"export ROS_MASTER_URI=http://localhost:11311\" >> ~/.bashrc"
+# sh -c "echo \"export ROS_MASTER_URI=http://localhost:11311\" >> ~/.bashrc"  # master, roscore deleted
 # sh -c "echo \"export ROS_HOSTNAME=localhost\" >> ~/.bashrc"
 
 source $HOME/.bashrc
 
 
 # Added - Install Turtlesim
-echo "[Install Turtlesim]"
+echo "[Install turtlesim]"
 sudo apt install ros-dashing-turtlesim -y
 
 
